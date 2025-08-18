c8260fcf61b5d34a923b10a8357cb9dba958e993
foxosdev9 <chentoufanass3@gmail.com>
2025-08-18 16:58:45 +0100

    small problem

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ if(process.env.NODE_ENV === 'developement') app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.get('/favicons', (req, res) => res.status(204));
 app.get('/', (req, res) => { res.send('<h1>WELCOME TO BI3 API 😂</h1>')})
 
 app.use('/api/v1/users', userRouter);
