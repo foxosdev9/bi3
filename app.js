f438044e15059adda69ba5c3b34597739af31783
foxosdev9 <chentoufanass3@gmail.com>
2025-08-25 22:13:08 +0100

    start handling error in express

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ app.use('/api/v1/products', productRouter);
 app.get('/favicons', (req, res) => res.status(204));
 app.get('/', (req, res) => { res.send('<h1>WELCOME TO BI3 API 😂</h1>')})
 
+app.all("/{*any}", (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.statusCode = 404;
+    err.status = 'fail';
+    next(err);
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // })
+});
 
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
 
 module.exports = app;
--- models/productModel.js
+++ models/productModel.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const prodSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String,
+        required: true,
+        minLength: [20, 'Name must be a less than 20 caracteres'],
+        // validate: [validator.isAlpha, 'Please name should not contain number']
+    },
     slug: String,
     price: Number,
-    categories: String,
+    categories: {
+        type: String,
+        enum: {
+            values: ['fasion', 'car', 'sex', 'phones'],
+            message: 'Please choose available catgories : FASION/CAR/SEX/PHONES'
+        }
+    },
     stock: Number,
     description: String,
     secureProduct: { type: Boolean, default: false},
@@ -28,6 +40,13 @@ prodSchema.pre('save', function(next){
 prodSchema.pre(/^find/, function(next){
     this.find({ secureProduct: { $ne: true}});
     next();
+});
+
+prodSchema.pre('aggregate', function(next){
+    this.pipeline().unshift( { $match: { secureProduct: {$ne: true}}});
+    //pipeline howa khat anabib li 7addna f had agregate pipline() method return arr of our pipline
+    // console.log(this.pipeline)
+    next();
 })
 
 prodSchema.post('save', function(doc, next){
@@ -42,3 +61,26 @@ prodSchema.virtual('username').get(function(){
 const productModel = mongoose.model('Product', prodSchema);
 
 module.exports = productModel;
+
+
+//VALIDATION
+// GAL JONAS BLI SIR F BACKEND HOWA MANKHALIWCH DATA LI KATJI MN USER TSAJEL NICHAN F DB
+// KHASNA NT7A9O MANHA O NDIRO LIHA 3AMALIYAT TATHIIR
+// REQUIRED: HADA BUILD IN VALIDATOR
+// UNIQUE / MACHI VALIDATOR FIH AKHTA2
+// maxLength: [ 40, 'error message'] : hada validator build in kaikhdem m3a string
+// findByIdAndUpdate({ runValidators: false}) // maghadich ykhdem validator ila badlti name khas had property t3tiha true ila bghiti ykhdem
+// kaina method akhra bach tkteb build in validator enum: { values: [], message: ''} :: enum work just with STRING
+// min and max work with NUMBER / DATES : not string
+
+/**
+ * CUSTOM VALIDATION
+ * HOMA VALIDATOR KAN7THOM HNA F SCHEMA
+ * validate: {
+ *    validator: function(val){}, // this keyword katkhdem gha fash kansjlo new document
+ *     message: 'ERROR MESSAGE'
+ * }
+ * or 
+ * validate: [function, 'error message']
+ * ist3mel maktaba validator.js
+ */
