6cf0b78463e9b0cc73e43e5c04d23478da2be794
foxosdev9 <chentoufanass3@gmail.com>
2025-09-01 18:38:47 +0100

    llahysabrni

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const productRouter = require('./routes/productRouter');
 
 if(process.env.NODE_ENV === 'developement') app.use(morgan('dev'));
 
-app.use(cors({ origin: 'https://bi3-morocco.netlify.app', credentials: true }));
+app.use(cors({ origin: ['https://bi3-morocco.netlify.app', 'http://localhost:5173'], credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
